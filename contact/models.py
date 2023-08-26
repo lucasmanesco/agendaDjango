6c9bc4703f9acfed9903bfa91612d3aecd009be6
lucasmanesco <lucas_manesco@hotmail.com>
2023-08-26 20:34:50 -0300

    estudos-26-08

--- contact/models.py
+++ contact/models.py
@@ -16,6 +16,8 @@ class Contact(models.Model):
     email = models.EmailField(max_length=50, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     decription = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m')
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
