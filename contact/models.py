ad9383987afaf4c4b710c3fe39185eae9d687cad
lucasmanesco <lucas_manesco@hotmail.com>
2023-08-28 22:10:46 -0300

    estudos-28-08

--- contact/models.py
+++ contact/models.py
@@ -9,6 +9,13 @@ from django.utils import timezone
 # picture (image)
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
@@ -18,6 +25,11 @@ class Contact(models.Model):
     decription = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m')
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
